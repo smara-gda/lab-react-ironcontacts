83169cbe8384e07f40ae82eb583cf193a4918d14
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2021-03-02 21:02:32 +0100

    iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,35 @@ class App extends React.Component{
     super()
   
     this.state = {
-      contacts: contacts.slice(0,5)
+      contactsArr: contacts.slice(0,5)
     }
   }
 
+  // iteration 2
+  addContact = () => {
+    const uniqueContacts = contacts.filter(eachContact => {
+      return !this.state.contactsArr.includes(eachContact)
+    })
+    
+    if(uniqueContacts.length > 0) {
+      const randomContact = uniqueContacts[Math.floor(Math.random()*uniqueContacts.length)]
+      this.setState({
+        contactsArr : [...this.state.contactsArr, randomContact]
+      })
+    }
+  }
+// Iteration 2
+  // handleButtonClick= ()=>{
+  //   this.setState(
+  //     {contactsArr: this.state.uniqueContacts}
+  //   )
+  // }
 
   render(){
     return(
       <div>
         <h1>IronContacts </h1>
+        <button onClick={this.addContact}>Add a random contact</button>
         <table>
           <thead>
             <tr>
@@ -26,7 +46,7 @@ class App extends React.Component{
              </tr>
           </thead>
           <tbody>
-             {this.state.contacts.map(contact => {
+             {this.state.contactsArr.map(contact => {
                return (
                   <tr key={ contact.id}>   
                     <td><img src={contact.pictureUrl} style={ { width: '50px'}} alt=""/></td>
